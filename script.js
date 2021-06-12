0f14d27bb8b91b920f7348fb9dde16bacdc34e7e
mondalraj <mondal021002rajib@gmail.com>
2021-06-12 17:39:54 +0530

    v2.0

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const body = document.querySelector('body');
 
 const header = document.querySelector('header > h1');
 
+const copyright = document.querySelector('.copyright');
+
 toggle.addEventListener('click', () => {
     toggle.classList.toggle('active');
     body.classList.toggle('active');
     header.classList.toggle('active');
-
-})
+    copyright.classList.toggle('active');
+});
